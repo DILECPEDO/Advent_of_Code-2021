af640f4965a972b893eef17a0f59daad036b32fe
Dilec Padovani <dilec.padovani@gmail.com>
2021-12-30 15:55:10 +0100

    Finished day 3

--- _3/src/main.rs
+++ _3/src/main.rs
@@ -13,7 +13,7 @@ fn get_numbers_from_file(input_file: &str) -> Vec<String> {
     lines.into_iter().map(|line| line.unwrap()).collect()
 }
 
-fn get_o_and_1_in_nth_place(numbers_string: &Vec<String>, index: usize) -> (usize, usize) {
+fn get_0_and_1_in_nth_place(numbers_string: &Vec<String>, index: usize) -> (usize, usize) {
     let digits = numbers_string
         .iter()
         .map(|s| s.chars().nth(index).unwrap())
@@ -30,12 +30,18 @@ fn get_o_and_1_in_nth_place(numbers_string: &Vec<String>, index: usize) -> (usiz
         .len();
     (_0s, _1s)
 }
+
+fn bin_string_to_i32(string: &String) -> i32 {
+    i32::from_str_radix(&string, 2).expect("Not a binary number!")
+}
 fn main() {
     let numbers_string = get_numbers_from_file("input.txt");
+
+    // PART ONE
     let mut gamma_rate = String::from("");
     let mut epsilon_rate = String::from("");
     for i in 0..numbers_string.first().unwrap().len() {
-        let (_0s, _1s) = get_o_and_1_in_nth_place(&numbers_string, i);
+        let (_0s, _1s) = get_0_and_1_in_nth_place(&numbers_string, i);
         assert_eq!(numbers_string.len(), _0s + _1s);
         if _1s > _0s {
             gamma_rate = format!("{}{}", gamma_rate, "1");
@@ -45,8 +51,57 @@ fn main() {
             epsilon_rate = format!("{}{}", epsilon_rate, "1");
         }
     }
-    let gamma_rate = i32::from_str_radix(&gamma_rate, 2).expect("Not a binary number!");
-    let epsilon_rate = i32::from_str_radix(&epsilon_rate, 2).expect("Not a binary number!");
+    let gamma_rate = bin_string_to_i32(&gamma_rate);
+    let epsilon_rate = bin_string_to_i32(&epsilon_rate);
     let result = gamma_rate * epsilon_rate;
-    println!("{}", result);
+    println!("power consumption: {}", result);
+
+    // PART TWO
+    part_two(numbers_string);
+}
+enum Element {
+    Oxigen,
+    CO2,
+}
+
+fn part_two(numbers_string: Vec<String>) {
+    let ox_rating = find_rating(numbers_string.to_owned(), 0, Element::Oxigen);
+    let ox_rating = ox_rating.first().unwrap();
+    let co2_rating = find_rating(numbers_string.to_owned(), 0, Element::CO2);
+    let co2_rating = co2_rating.first().unwrap();
+    let result = bin_string_to_i32(&ox_rating) * bin_string_to_i32(&co2_rating);
+    println!("life support rating: {}", result);
+}
+
+fn find_rating(numbers_string: Vec<String>, index: usize, element: Element) -> Vec<String> {
+    assert!(
+        index <= numbers_string.first().unwrap().len(),
+        "Index is larger then bynary number"
+    );
+    if numbers_string.len() > 1 {
+        let (_0s, _1s) = get_0_and_1_in_nth_place(&numbers_string, index);
+        let common = match element {
+            Element::Oxigen => {
+                if _0s > _1s {
+                    "0"
+                } else {
+                    "1"
+                }
+            }
+            Element::CO2 => {
+                if _0s <= _1s {
+                    "0"
+                } else {
+                    "1"
+                }
+            }
+        };
+        let numbers_string = numbers_string
+            .into_iter()
+            .filter(|s| s.chars().nth(index).unwrap().to_string() == common)
+            .collect::<Vec<String>>();
+        return find_rating(numbers_string, index + 1, element);
+    } else {
+        numbers_string
+    }
 }
