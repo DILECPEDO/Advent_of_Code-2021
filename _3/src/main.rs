c500e1226caa64ec1fe7baf533c7e3af316c7ba9
Dilec Padovani <dilec.padovani@gmail.com>
2021-12-30 14:45:46 +0100

    reformated code

--- _3/src/main.rs
+++ _3/src/main.rs
@@ -13,25 +13,29 @@ fn get_numbers_from_file(input_file: &str) -> Vec<String> {
     lines.into_iter().map(|line| line.unwrap()).collect()
 }
 
+fn get_o_and_1_in_nth_place(numbers_string: &Vec<String>, index: usize) -> (usize, usize) {
+    let digits = numbers_string
+        .iter()
+        .map(|s| s.chars().nth(index).unwrap())
+        .collect::<Vec<char>>();
+    let _0s = digits
+        .iter()
+        .filter(|c| **c == '0' as char)
+        .collect::<Vec<&char>>()
+        .len();
+    let _1s = digits
+        .iter()
+        .filter(|c| **c == '1' as char)
+        .collect::<Vec<&char>>()
+        .len();
+    (_0s, _1s)
+}
 fn main() {
     let numbers_string = get_numbers_from_file("input.txt");
     let mut gamma_rate = String::from("");
     let mut epsilon_rate = String::from("");
     for i in 0..numbers_string.first().unwrap().len() {
-        let digits = numbers_string
-            .iter()
-            .map(|s| s.chars().nth(i).unwrap())
-            .collect::<Vec<char>>();
-        let _0s = digits
-            .iter()
-            .filter(|c| **c == '0' as char)
-            .collect::<Vec<&char>>()
-            .len();
-        let _1s = digits
-            .iter()
-            .filter(|c| **c == '1' as char)
-            .collect::<Vec<&char>>()
-            .len();
+        let (_0s, _1s) = get_o_and_1_in_nth_place(&numbers_string, i);
         assert_eq!(numbers_string.len(), _0s + _1s);
         if _1s > _0s {
             gamma_rate = format!("{}{}", gamma_rate, "1");
